4fbbbfa29ee4f973313bd898492ee0c93a890804
Emma K Alexandra <emma@emma.sh>
2020-01-21 15:28:32 -0500

    v1 (#1)
    * Check in
    
    * Update RelativeDateViewModel.swift
    
    * Update RelativeDateViewModel.swift
    
    * Update RelativeDateViewModel.swift
    
    * Update RelativeDateViewModel.swift
    
    * Update RelativeDateViewModel.swift
    
    * Update RelativeDateViewModel.swift
    
    * Update RelativeDateViewModel.swift
    
    * this is wild
    
    * Update RelativeDateView.swift
    
    * Use DateHelper
    
    Update less
    
    * Refresh view every second
    
    * isPast, isNow and isFuture optional bindings
    
    * Use binding rather than exact value for date
    
    * Update isfuture, etc bindings on model init
    
    * Docs, README

--- Sources/RelativeDateView/RelativeDateView.swift
+++ Sources/RelativeDateView/RelativeDateView.swift
@@ -1,15 +1,77 @@
+//
+//  RelativeDateViewModel.swift
+//
+//
+//  Created by Emma K Alexandra on 1/12/20.
+//
+
 import SwiftUI
+import DateHelper
 
 public struct RelativeDateView: View {
+    @State private var formattedDate: String = ""
     @ObservedObject var viewModel: RelativeDateViewModel
     
-    public init(date: Date) {
-        self.viewModel = RelativeDateViewModel(date: date)
+    /// If the current date is in the future
+    @Binding var isFuture: Bool
+    
+    /// If the current date is right now, plus or minus 10 seconds or so
+    @Binding var isNow: Bool
+    
+    /// If the current date is in the past
+    @Binding var isPast: Bool
+    
+    /// Creates a RelativeDateView
+    ///
+    /// - parameter date: Date to display a relative time to
+    /// - parameter format: Optional. Strings to display when a date is certain amounts of time away.
+    /// - parameter isFuture: If the current date is in the future
+    /// - parameter isNow: If the current date is right now, plus or minus 10 seconds or so
+    /// - parameter isPast: If the current date is in the past
+    public init(date: Binding<Date>, format: [RelativeTimeStringType: String]? = nil, isFuture: Binding<Bool>? = nil, isNow: Binding<Bool>? = nil, isPast: Binding<Bool>? = nil) {
+        self.viewModel = RelativeDateViewModel(date: date.wrappedValue, format: format)
+        
+        if let isFuture = isFuture {
+            self._isFuture = isFuture
+            
+        } else {
+            self._isFuture = Binding(get: {
+                false
+            }, set: { _ in })
+            
+        }
+        
+        if let isNow = isNow {
+            self._isNow = isNow
+            
+        } else {
+            self._isNow = Binding(get: {
+                false
+            }, set: { _ in })
+            
+        }
+        
+        if let isPast = isPast {
+            self._isPast = isPast
+            
+        } else {
+            self._isPast = Binding(get: {
+                false
+            }, set: { _ in })
+            
+        }
         
     }
     
     public var body: some View {
         Text(self.viewModel.formattedDate)
+            .onReceive(Timer.publish(every: 1, on: .main, in: .default).autoconnect()) {
+                self.formattedDate = String(describing: $0)
+                self.isFuture = self.viewModel.isFuture
+                self.isNow = self.viewModel.isNow
+                self.isPast = self.viewModel.isPast
+
+        }
         
     }
     
@@ -18,7 +80,7 @@ public struct RelativeDateView: View {
 #if DEBUG
 struct RelativeDateView_Previews: PreviewProvider {
     static var previews: some View {
-        RelativeDateView(date: Date())
+        RelativeDateView(date: .constant(Date()))
         
     }
     
--- Sources/RelativeDateView/RelativeDateViewModel.swift
+++ Sources/RelativeDateView/RelativeDateViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  RelativeDateViewModel.swift
 //  
 //
 //  Created by Emma K Alexandra on 1/12/20.
@@ -7,24 +7,90 @@
 
 import SwiftUI
 import Combine
+import DateHelper
 
 class RelativeDateViewModel: ObservableObject {
+    /// Cancellable for timer publisher
+    private var assignCancellable: AnyCancellable? = nil
+    
+    /// Date to display relative time to
     var date: Date {
         didSet {
-            self.formattedDate = self.formatter.localizedString(for: self.date, relativeTo: Date())
-            print(self.formattedDate)
+            self.isFutureNowOrPast()
+            
+            self.formattedDate = self.formatDate(self.date)
             
         }
         
     }
-    let formatter = RelativeDateTimeFormatter()
     
-    init(date: Date) {
+    /// What Strings to display for certain RelativeTimeStringTypes
+    var format: [RelativeTimeStringType: String]?
+    
+    /// Creates RelativeDateViewModel.
+    ///
+    /// - parameter date: Date to display relative time to
+    /// - parameter format: Date formats to display
+    init(date: Date, format: [RelativeTimeStringType: String]?) {
         self.date = date
+        self.format = format
         
-        self.formatter.unitsStyle = .full
+        // Start timer
+        self.assignCancellable = Timer.publish(every: 1, on: .main, in: .default)
+            .autoconnect()
+            .map { _ in
+                self.isFutureNowOrPast()
+                
+                return self.formatDate(self.date)
+            
+            }
+            .assign(to: \RelativeDateViewModel.formattedDate, on: self)
+        
+        self.isFutureNowOrPast()
         
     }
     
+    /// Format given date to a relative time string
+    /// - parameter date: Date to convert to a relative time string
+    /// - returns: Relative time stirng
+    func formatDate(_ date: Date) -> String {
+        return self.date.toStringWithRelativeTime(strings: self.format)
+        
+    }
+    
+    /// Sets isFuture/isNow/isPast based on the date's RelativeTimeStringType
+    func isFutureNowOrPast() {
+        switch self.date.toRelativeTime() {
+        case .nowPast:
+            self.isFuture = false
+            self.isNow = true
+            self.isPast = true
+        case .nowFuture:
+            self.isFuture = true
+            self.isNow = true
+            self.isPast = false
+        case .secondsPast, .oneMinutePast, .minutesPast, .oneHourPast, .hoursPast, .oneDayPast, .daysPast, .oneWeekPast, .weeksPast, .oneMonthPast, .monthsPast, .oneYearPast, .yearsPast:
+            self.isPast = true
+            self.isNow = false
+            self.isFuture = false
+        case .secondsFuture, .oneMinuteFuture, .minutesFuture, .oneHourFuture, .hoursFuture, .oneDayFuture, .daysFuture, .oneWeekFuture, .weeksFuture, .oneMonthFuture, .monthsFuture, .oneYearFuture, .yearsFuture:
+            self.isPast = false
+            self.isNow = false
+            self.isFuture = true
+        }
+        
+    }
+    
+    /// The date formatted in relative time
     @Published var formattedDate: String = ""
+    
+    /// If the current date is in the future
+    @Published var isFuture: Bool = false
+    
+    /// If the current date is now, plus or minus 10 seconds or so
+    @Published var isNow: Bool = false
+    
+    /// If the current date is in the past
+    @Published var isPast: Bool = false
+    
 }
